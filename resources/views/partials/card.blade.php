fdfbcdcc0d5b9988beb99c93d1eb61acce08f2c5
MatteoAgu01 <matteoaguiari012@gmail.com>
2023-05-23 00:18:10 +0200

    first commit

---
+++ resources/views/partials/card.blade.php
@@ -0,0 +1,21 @@
+
+	<div class="card d-flex  bg-dark m-5">
+		<div>
+			<img src="{{ $product['thumb'] }}">
+		</div>
+		<div>
+			<span class="text-white text-uppercase"></span>
+		</div>
+	</div>
+
+
+
+<style lang="scss" scoped>
+img{
+width: 100%;
+height: calc(100vh / 2);
+}
+
+
+
+</style>
